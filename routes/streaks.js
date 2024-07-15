c1eefecb9a727f24214db4e20dedfcaac87e27c9
Saood Ahmad <99206626+saoodchoudhary@users.noreply.github.com>
2024-07-15 17:56:37 +0530

    streaks update

--- routes/streaks.js
+++ routes/streaks.js
@@ -1,9 +1,11 @@
 
 
-const router = require('express').Router();const { handleGetStreakAndRestoreDates } = require('../controller/streaks');
+const router = require('express').Router();const { handleGetStreakAndRestoreDates, handlePostRestoreStreaks } = require('../controller/streaks');
 
 
 router.post('/getStreakAndRestoreDates', handleGetStreakAndRestoreDates);
+
+router.post('/restore', handlePostRestoreStreaks);
 
 
 module.exports = router;
